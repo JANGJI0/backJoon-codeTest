f65e3bf36356472a0a852b3a38bfa05a2428607a
user <user@DESKTOP-MCPVQEB>
2025-04-11 22:15:16 +0900

    1001번 문제 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램 작성 1000번 문제랑 똑같아서 복습하는 개념으로 시작

---
+++ backJoonTest/src/backJoonTest/Bj1000.java
@@ -0,0 +1,12 @@
+package backJoonTest;
+import java.util.Scanner; // java packge의 util 디렉토리 안에 있는 Scanner 클래스를 임포트해서 사용
+public class Bj1000 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); // Scanner 객체명 = new Scanner(System.in);
+    // System.in이란 사용자로부터 키 입력을 받기 위해 사용함(표준입력)
+    // 입력받은 값은 Byte 단위로 읽음
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println(a + b);    
+    }
+}
