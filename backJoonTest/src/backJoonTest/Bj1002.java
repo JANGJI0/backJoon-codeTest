6f682d8c1a17da177dc0f6ace9bf67eeac5f173b
user <user@DESKTOP-MCPVQEB>
2025-04-12 23:16:28 +0900

    1002번 문제 A * B 를 구하여라 전 문제들에서 * 만 바꾸면 되서 복습차원으로 해봤다. 어렵긴하다

---
+++ backJoonTest/src/backJoonTest/Bj1002.java
@@ -0,0 +1,15 @@
+package backJoonTest;
+import java.util.Scanner; // java packge 의 util 디렉토리 안에 있는 Scanner 클래스를 import 해서 사용
+public class Bj1002 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); // Scanner 객체명 = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a * b);; // nextInt() 가 입력을 기다리고 있는 상태, 실행하고 콘솔창에 숫자를 입력을 해야 값이 나온다.
+		
+		
+	}
+
+}
