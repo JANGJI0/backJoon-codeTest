f542c6ab926461acbcfa81a71c6e934ae82aaedb
user <user@DESKTOP-MCPVQEB>
2025-04-14 21:11:20 +0900

    1004번 문제 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 지금까지 배운 내용을 한번에 출력하는것으로 안보고 작성했다.

---
+++ backJoonTest/src/backJoonTest/Bj1004.java
@@ -0,0 +1,17 @@
+package backJoonTest;
+import java.util.Scanner;
+public class Bj1004 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A =sc.nextInt();
+		int B =sc.nextInt();
+		System.out.println(A + B);
+		System.out.println(A - B);
+		System.out.println(A * B);
+		System.out.println(A / B);
+		System.out.println(A % B);
+		
+		// 1~3번까지의 내용을 한번에 하는 문제로 안보고 했다.
+	}
+}
