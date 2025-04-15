522bc29b43061837a20e92c38091221bd5ba7575
user <user@DESKTOP-MCPVQEB>
2025-04-15 23:06:58 +0900

    와... Scanner은 무조건 써야되는걸 몰랐다...

---
+++ backJoonTest/src/backJoonTest/Bj10926.java
@@ -0,0 +1,14 @@
+package backJoonTest;
+import java.util.Scanner; // Scanner은 무조건 써야하는듯하다
+public class Bj10926 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String add = "??!";
+		
+		String id = sc.next();
+		System.out.println(id + add);
+
+	}
+
+}
