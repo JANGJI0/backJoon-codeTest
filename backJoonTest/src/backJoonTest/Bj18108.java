edd0558f3a7978ffe0f7c7f83a14817f8ef6d39c
user <user@DESKTOP-MCPVQEB>
2025-04-16 23:04:33 +0900

    18108번 문제 1998년생인 내가 태국에서는 2541년생? 입력 2541 -> 출력 1998
    import, 선언, 출력까지 잘 한것 같다
    하지만 이게 아주 기초중에 기초란말이지...

---
+++ backJoonTest/src/backJoonTest/Bj18108.java
@@ -0,0 +1,17 @@
+// 1998년생인 내가 태국에서는 2541년생?
+// 예제입력 2541 -> 출력 1998
+
+package backJoonTest;
+import java.util.Scanner;
+public class Bj18108 {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	int y = sc.nextInt();
+	
+	System.out.println(y - 543);
+	
+
+	}
+	
+}
