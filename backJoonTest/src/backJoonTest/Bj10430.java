ec2ee13b03e8e0bbd017fb6c18b62823bc920498
user <user@DESKTOP-MCPVQEB>
2025-04-17 23:13:34 +0900

    10430번 문제 // (A + B%는 ((A%c) + (B%))%C 와 같을까? // (A + B%는 ((A%c) x (B%))%C 와 같을까? // 세 수 A, B, C가 주어졌을 때 위의 네 가지 값을 구하는  프로그램을 작성하시오.
    이제 원리가 이해가 되기 시작한다... import 하고 new생성하고 선언하고 출력...

---
+++ backJoonTest/src/backJoonTest/Bj10430.java
@@ -0,0 +1,22 @@
+package backJoonTest;
+import java.util.Scanner;
+public class Bj10430 {
+// (A + B%는 ((A%c) + (B%))%C 와 같을까?
+// (A + B%는 ((A%c) x (B%))%C 와 같을까?
+// 세 수 A, B, C가 주어졌을 때 위의 네 가지 값을 구하는  프로그램을 작성하시오.
+	public static void main(String[] agrs) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+
+		
+		System.out.println((A + B) % C);
+		System.out.println(((A % C) + (B % C)) % C);
+		System.out.println((A * B) % C);
+		System.out.println(((A % C) * (B % C)) % C);
+		
+	}
+
+}
