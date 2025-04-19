cec751d7edc2f358b3c7a783df0ef59f76a4dd54
user <user@DESKTOP-MCPVQEB>
2025-04-19 23:13:56 +0900

    11382번 문제 꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!  첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 1012)이 공백을 사이에 두고 주어진다.
     처음에 왜 이렇게 쉽지 하다가 큰코 다졌다... 자꾸 런타임오류가 뜨길레 결국 구글링을 해버렸다..
    
     받는 숫자가 int형보다 큰 값을 출력하므로 long형(8byte)을 사용해야만 한다...

---
+++ backJoonTest/src/backJoonTest/Bj11382.java
@@ -0,0 +1,18 @@
+// 꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
+// 첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 1012)이 공백을 사이에 두고 주어진다.
+
+package backJoonTest;
+import java.util.Scanner;
+public class Bj11382 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// 받는 숫자가 int현 보다 큰 값을 출력하므로 long형(8byte)을 사용하여 출력합니다.
+		// intㄹ 할 경우 런타임오류가 뜬다.
+		long A = sc.nextLong();
+		long B = sc.nextLong();
+		long C = sc.nextLong();
+		
+		System.out.println(A + B + C);
+	}
+}
