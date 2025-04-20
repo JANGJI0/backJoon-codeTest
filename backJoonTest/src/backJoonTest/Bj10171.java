322c00f380b441568c6d04f4bdf622f4ab3964d8
user <user@DESKTOP-MCPVQEB>
2025-04-20 23:48:20 +0900

    2025.04.20 고양이 문제 (한줄평 : 감도 안잡혔다.. 이스케이프 시퀀스는 자격증공부때 얼핏 본것 같았다.)
    // 아래 예제와 같이 고양이를 출력하시오.
    // \    /\
    //  )  ( ')
    // (  /  )
    //  \(__)|
    
    재능이 없는건가...

---
+++ backJoonTest/src/backJoonTest/Bj10171.java
@@ -0,0 +1,35 @@
+// 아래 예제와 같이 고양이를 출력하시오.
+// \    /\
+//  )  ( ')
+// (  /  )
+//  \(__)|
+
+package backJoonTest;
+
+public class Bj10171 {
+    public static void main(String[] args) {
+        
+        System.out.println("\\    /\\");
+        System.out.println(" )   ( ')");
+        System.out.println("(   /  ) ");
+        System.out.println(" \\(__)|");
+        
+        
+        // 이건 어려웠다... 아예 감이 안잡혔다..
+        
+        /*
+        	이스케이프 시퀀스(escape sequences) 란?
+         	Java에서 일련의 문자에 대한 해석을 위해 백슬래시가 앞에 오는 문자를 말한다.
+        	Java 컴파일러는 특별한 의미가 있는 하나의 단일 문자로 이스케이프 시퀀스를 사용한다
+        	
+        		입력	출력
+        		\t	탭(띄어쓰기) 을 삽입합니다
+        		\n	새 줄(줄바꿈) 을 삽입합니다
+        		\r	커서를 맨 앞으로 되돌립니다
+        		\'	' 를 출력합니다
+        		\"	" 를 출력합니다
+        		\	\ 를 출력합니다
+       */
+        		 
+    }
+}
