f2a85624a27561f5ac42fe49b1bbf048a7a47258
user <user@DESKTOP-MCPVQEB>
2025-04-22 23:26:51 +0900

    2025.04.22 Bj1330번 문제 (감은 잡힐듯말듯하다.)
    if문을 써야되는지가 바로바로 나와야하는데 좀 생각하니까 나왔다..

---
+++ backJoonTest/src/backJoonTest/Bj1330.java
@@ -0,0 +1,28 @@
+package backJoonTest;
+// 두 수 비교하고
+// 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+// 첫째 줄에 다음 세 가지 중 하나를 출력한다.
+
+// A가 B보다 큰 경우에는 '>'를 출력한다.
+// A가 B보다 작은 경우에는 '<'를 출력한다.
+// A와 B가 같은 경우에는 '=='를 출력한다.
+
+import java.util.Scanner;
+public class Bj1330 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		// 자원 정리
+		sc.close();
+		
+		if(A>B) System.out.println(">");
+		else if(A<B) System.out.println("<");
+		else System.out.println("==");
+		
+		
+		
+	}
+}
