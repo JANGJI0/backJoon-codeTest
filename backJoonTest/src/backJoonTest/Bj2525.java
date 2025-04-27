2f3cf2fb6ffc803a64ef9d399222f4180ed07b1a
user <user@DESKTOP-MCPVQEB>
2025-04-27 23:42:29 +0900

    2025.04.27 백준 2525번 (알람시계를 했는데 어렵다..)
    하.. 알고리즘이 이렇게나 어려운건가... 재능이없는건가...

---
+++ backJoonTest/src/backJoonTest/Bj2525.java
@@ -0,0 +1,26 @@
+// KOI 전자에서는 건강에 좋고 맛있는 훈제오리구이 요리를 간편하게 만드는 인공지능 오븐을 개발하려고 한다. 인공지능 오븐을 사용하는 방법은 적당한 양의 오리 훈제 재료를 인공지능 오븐에 넣으면 된다. 그러면 인공지능 오븐은 오븐구이가 끝나는 시간을 분 단위로 자동적으로 계산한다.
+// 또한, KOI 전자의 인공지능 오븐 앞면에는 사용자에게 훈제오리구이 요리가 끝나는 시각을 알려 주는 디지털 시계가 있다.
+// 훈제오리구이를 시작하는 시각과 오븐구이를 하는 데 필요한 시간이 분단위로 주어졌을 때, 오븐구이가 끝나는 시각을 계산하는 프로그램을 작성하시오.
+
+// 첫째 줄에 종료되는 시각의 시와 분을 공백을 사이에 두고 출력한다. (단, 시는 0부터 23까지의 정수, 분은 0부터 59까지의 정수이다. 디지털 시계는 23시 59분에서 1분이 지나면 0시 0분이 된다.)
+package backJoonTest;
+import java.util.Scanner;
+public class Bj2525 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		int C = sc.nextInt();
+		
+		int min = A * 60 + B; // min = 60 * 시 *분
+		min += C;			// 요리하는데 걸린 시간 더하기
+		
+		int hour = (min / 60) % 24; // 시(24시 이상이 될 경우 0시부터 시작
+		int minute = min % 60; // 분
+		
+		System.out.println(hour + " " + minute);
+		
+	}
+}
