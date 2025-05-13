04954608944c3c89a887c2b1cc7bec50b63f9d74
JANGJI0 <qlvm2002@gmail.com>
2025-05-13 23:14:32 +0900

    2025.05.13 백준 2439번 별찍기-2문제 (...별거 없을줄 알고 자신만만하게 별찍기 -1이랑 비슷하게하고 --, 공백, 등등.. 했는데도 안된다... 결국 구글링 해버렸더니... N-i, 추가 반복문... 나를 반겼다.. 와.. 너무 어렵다.. 진짜 재능이 없는걸까...

---
+++ backJoonTest/src/backJoonTest/Bj29_2439.java
@@ -0,0 +1,36 @@
+package backJoonTest;
+/*
+ * 별찍기 -2
+ * 문제 : 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+ * 		하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오
+ * 입력 : 첫째 줄에 N(1<= N <=100)이 주어진다.
+ * 출력 : 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+ */
+import java.util.Scanner;
+public class Bj29_2439 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// 테스트 개수 입력
+		int N = sc.nextInt();
+		
+		for(int i = 1; i <= N; i++) {
+			
+			// 1. 공백을 출력(N - i개 만큼)
+			for(int j = 1; j <= N - i; j++) {
+				System.out.print(" ");
+			}
+			
+			// 2. 그 다음 별 출력(i개 만큼)
+			for(int k = 1; k <= i; k++) {
+				System.out.print("*");
+			}
+			// 줄 바꿈
+			System.out.println();
+		}
+		
+		// 자원닫기
+		sc.close();
+	}
+
+}
