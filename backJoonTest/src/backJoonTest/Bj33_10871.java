ffbc01dd967cf3cea4dc473b42d454b5d6e9f82f
JANGJI0 <qlvm2002@gmail.com>
2025-05-18 23:32:09 +0900

    2025.05.18 백준 10871번 X보다 작은수(저번 문제랑 별다를게 없어서 for문 까지 무난하게 왔다. if문 까지와서 A<x 까지 적고 뭔가 저번문제보다 이상한거같아서 저번문제보면서 했더니 아예 다른거였다.. 내가 처음에 했던게 맞았다..

---
+++ backJoonTest/src/backJoonTest/Bj33_10871.java
@@ -0,0 +1,30 @@
+package backJoonTest;
+/*
+ * X보다 작은 수
+ * 문제 : 정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때 A에서 Xqhek 작은 수를 모두 출력하는 프로그램을 작성하시오.
+ * 입력 : 첫째 줄에 N과 X가 주어진다.(1<= N, X<= 10,000)
+ * 		 둘째 줄에 수열 A를 이루는 정수 N개가 주어진다. 주어지는 정수는 모두 1보다 크거나 같고, 10,000보다 작거나 같은 정수이다.
+ * 출력 : X보다 작은 수를 입력 받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
+ */
+import java.util.Scanner;
+public class Bj33_10871 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// N번째, X보다 작은 정수 입력
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		
+		// N개의 수를 하나씩 입력 받으면서 x보다 작으면 출력
+		for(int i = 0; i < N; i++) {
+			int A = sc.nextInt();
+		
+			if(A < X) {
+				System.out.print(A + " ");
+			}	
+		}
+		// 자원닫기
+		sc.close();
+	}
+
+}
